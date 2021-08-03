17deb67508bf9cf8b9969df2c407a1e15697ba28
jurgob <jurgo.boemo@gmail.com>
2021-08-03 19:32:08 +0100

    add audio into record file

--- lesson_01__enter_the_mic.js
+++ lesson_01__enter_the_mic.js
@@ -1,25 +1,35 @@
-
-
 const mic = require('mic');
-
 const fs = require('fs');
- 
-const micInstance = mic({
+
+const audioConfig = {
     rate: '16000',
-    channels: '1',
-    debug: true,
+    channels: '1'
+}
+
+const micInstance = mic({
+    rate: audioConfig.rate,
+    channels: audioConfig.channels,
+    debug: false,
     exitOnSilence: 6
 });
 
 const micInputStream = micInstance.getAudioStream();
 
-const outputFileStream = fs.WriteStream('output.raw');
+const fileName = 'output.raw'
+const outputFileStream = fs.WriteStream(fileName);
+
+micInputStream.pipe(outputFileStream);
 
 micInputStream.on('startComplete', function() {
-    console.log("Got SIGNAL startComplete");
+    console.log("Start recording");
     setTimeout(function() {
             micInstance.pause();
-    }, 5000);
+            console.log(`Record Completed`)
+            console.log(`To listen it run: `)
+            console.log(`play -b 16 -e signed -c ${audioConfig.channels} -r ${audioConfig.rate} ${fileName} `)
+            process.exit(0)
+            
+    }, 15000);
 });
 
 micInstance.start();
