cb09269f1745227f8637740d28e2e5ff7c054d49
jurgob <jurgo.boemo@gmail.com>
2021-08-04 01:28:40 +0100

    lesson 3 some more music

--- lesson_03__math_music_baudio.js
+++ lesson_03__math_music_baudio.js
@@ -6,48 +6,30 @@ const audioConfig = {
     channels: 1,
     bitDepth: 16
 }
-
 const speaker = new Speaker({
     sampleRate: audioConfig.rate,
     channels: audioConfig.channels,
     bitDepth: audioConfig.bitDepth
-  });
-
-  
-
-let nMusic1 = 0;
-function startYourEngine(t) {
-    const x = Math.sin(t * 262 + Math.sin(nMusic1));
-    nMusic1 += Math.sin(t);
-    return x;
-}
+});
 
 const tau = 2 * Math.PI
 const sin = (t, freq) => Math.sin(tau * t * freq)
-
-function sinWaveSingle(t) {
-    const x = sin(t, 400);
-    return x
+/* music effects*/
+const whiteNoise = (t) => Math.random() * (1 - -1) + -1;
+const sinWaveSingle = (t) => sin(t, 400)
+const sinWaves2ofThem = (t) => sin(t, 400) + sin(t, 404);
+const startYourEngine = (t) => {
+    if(!startYourEngine.n)
+        startYourEngine.n = 0
+    const x = Math.sin(t * 262 + Math.sin(startYourEngine.n));
+    startYourEngine.n += Math.sin(t);
+    return x;
 }
-
-
-function sinWaves2ofThem(t) {
-    const x = sin(t, 400) + sin(t, 404);
-    return x
-}
-
-function whiteNoise(t) {
-    return Math.random() * (1 - -1) + -1;
-}
-
-const musicList = [whiteNoise, startYourEngine, sinWaveSingle, sinWaveSingle, sinWaves2ofThem]
-
+const musicList = [whiteNoise, sinWaveSingle, sinWaveSingle, sinWaves2ofThem, startYourEngine]
 const musicIdx = process.argv[2] || 0
 const musicSelected = musicList[musicIdx]
 
 console.log(`Music Selected: ${musicSelected.name}`)
 
-
-const music = baudio(musicSelected);
-  
+const music = baudio(musicSelected);  
 music.pipe(speaker)
